a30a72bd1cd10504c7358e8727476efdff5bc3ff
jw910731 <jw910731@gmail.com>
2021-12-05 01:47:03 +0800

    Improve list implementation
    Discard pseudo node design and use nullptr instead.

--- list.h
+++ list.h
@@ -13,14 +13,13 @@ template <typename T> class list {
         friend class list;
 
       public:
-        node() = default;
-        template <typename... Args> node(Args &&...args) : element(args...) {}
+        node() : prev(nullptr), next(nullptr){};
+        template <typename... Args>
+        node(Args &&...args) : element(args...), prev(nullptr), next(nullptr) {}
     };
-    node *head, *pseudo_end;
+    node *head, *tail;
 
   public:
-    list() { head = pseudo_end = new node(); }
-
     class iterator : public std::iterator<std::bidirectional_iterator_tag, T> {
       private:
         iterator() = default;
@@ -62,29 +61,43 @@ template <typename T> class list {
      */
     template <typename... Args>
     iterator emplace(const iterator &it, Args &&...args) {
-        node *tmp = it.ptr->prev;
-        it.ptr->prev = new node(std::forward<Args>(args)...);
-        if (tmp) { // if append node in middle
-            tmp->next =
-                it.ptr->prev;    // update previous node's next node pointer
-        } else {                 // if append element before head
-            head = it.ptr->prev; // update head
+        if (it != iterator(nullptr)) {
+            node *tmp = it.ptr->prev;
+            it.ptr->prev = new node(std::forward<Args>(args)...);
+            if (tmp) { // if append node in middle
+                tmp->next =
+                    it.ptr->prev;    // update previous node's next node pointer
+            } else {                 // if append element before head
+                head = it.ptr->prev; // update head
+            }
+            auto ret = iterator(it.ptr->prev);
+            ret.ptr->next = it.ptr;
+            ret.ptr->prev = tmp;
+            return ret;
+        } else {
+            // position is null = emplace back
+            node *tmp = tail;
+            tail = new node(std::forward<Args>(args)...);
+            if (head == nullptr)
+                head = tail;
+            if (tmp != nullptr)
+                tmp->next = tail;
+            tail->prev = tmp;
+            return iterator(tail);
         }
-        auto ret = iterator(it.ptr->prev);
-        ret.ptr->next = it.ptr;
-        ret.ptr->prev = tmp;
-        return ret;
     }
 
     iterator begin() { return iterator(head); }
-    iterator end() { return iterator(pseudo_end); }
+    iterator end() { return iterator(nullptr); }
+
+    list() : head(nullptr), tail(nullptr) {}
 
     ~list() {
-        for (node *it = head, *tmp; it != pseudo_end; it = tmp) {
+        for (node *it = head, *tmp; it != tail; it = tmp) {
             tmp = it->next;
             delete it;
         }
-        delete pseudo_end;
+        delete tail;
     }
 };
 } // namespace mystl
