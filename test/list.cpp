3bcbb9565e1725a012f60172710ad4af1e195e81
jw910731 <jw910731@gmail.com>
2021-12-05 02:16:25 +0800

    Add constructor test and refactor unit tests

--- test/list.cpp
+++ test/list.cpp
@@ -6,36 +6,44 @@
 #include <list>
 #include <utility>
 
-void test_emplace() {
-    mystl::list<std::pair<int, int>> l;
-    std::list<std::pair<int, int>> testcase;
-
-    l.emplace(l.begin(), 10, 20);
-    testcase.emplace(testcase.begin(), 10, 20);
-    if (!std::equal(l.begin(), l.end(), testcase.begin(), testcase.end())) {
+void test_emplace_back(mystl::list<std::pair<int, int>> &l,
+                       std::list<std::pair<int, int>> &t) {
+    l.emplace_back(14, 24);
+    t.emplace_back(14, 24);
+    if (!std::equal(l.begin(), l.end(), t.begin(), t.end())) {
         abort();
     }
+}
 
+void test_emplace_front(mystl::list<std::pair<int, int>> &l,
+                        std::list<std::pair<int, int>> &t) {
+    l.emplace_front(10, 20);
+    t.emplace_front(10, 20);
+    if (!std::equal(l.begin(), l.end(), t.begin(), t.end())) {
+        abort();
+    }
+}
+
+void test_emplace(mystl::list<std::pair<int, int>> &l,
+                  std::list<std::pair<int, int>> &t) {
     l.emplace(std::next(l.begin(), 1), 11, 21);
-    testcase.emplace(std::next(testcase.begin(), 1), 11, 21);
-    if (!std::equal(l.begin(), l.end(), testcase.begin(), testcase.end())) {
+    t.emplace(std::next(t.begin(), 1), 11, 21);
+    if (!std::equal(l.begin(), l.end(), t.begin(), t.end())) {
         abort();
     }
 
     l.emplace(std::next(l.begin(), 2), 13, 23);
-    testcase.emplace(std::next(testcase.begin(), 2), 13, 23);
-    if (!std::equal(l.begin(), l.end(), testcase.begin(), testcase.end())) {
-        abort();
-    }
-
-    l.emplace(l.end(), 14, 24);
-    testcase.emplace(testcase.end(), 14, 24);
-    if (!std::equal(l.begin(), l.end(), testcase.begin(), testcase.end())) {
+    t.emplace(std::next(t.begin(), 2), 13, 23);
+    if (!std::equal(l.begin(), l.end(), t.begin(), t.end())) {
         abort();
     }
 }
 
 int main() {
-    test_emplace();
+    mystl::list<std::pair<int, int>> sample({{1, 11}, {2, 12}, {3, 13}});
+    std::list<std::pair<int, int>> testcase({{1, 11}, {2, 12}, {3, 13}});
+    test_emplace_front(sample, testcase);
+    test_emplace(sample, testcase);
+    test_emplace_back(sample, testcase);
     return 0;
 }
