ed8f32f2f9e50c94d42e3154921cac41135bb059
jw910731 <jw910731@gmail.com>
2021-12-05 19:34:23 +0800

    Implement size tracking and capacity query method
    size method is constant time complexity.

--- list.h
+++ list.h
@@ -23,7 +23,8 @@ template <typename T> class list {
         template <typename... Args>
         node(Args &&...args) : element(args...), prev(nullptr), next(nullptr) {}
     };
-    node *head, *tail;
+    node *m_head, *m_tail;
+    size_t m_size;
 
   public:
     class iterator : public std::iterator<std::bidirectional_iterator_tag, T> {
@@ -75,13 +76,14 @@ template <typename T> class list {
         it.ptr->prev = new node(std::forward<Args>(args)...);
         if (tmp) { // if append node in middle
             tmp->next =
-                it.ptr->prev;    // update previous node's next node pointer
-        } else {                 // if append element before head
-            head = it.ptr->prev; // update head
+                it.ptr->prev;      // update previous node's next node pointer
+        } else {                   // if append element before head
+            m_head = it.ptr->prev; // update head
         }
         auto ret = iterator(it.ptr->prev);
         ret.ptr->next = it.ptr;
         ret.ptr->prev = tmp;
+        ++m_size; // increment container size
         return ret;
     }
 
@@ -98,6 +100,7 @@ template <typename T> class list {
         it.ptr->next->prev = it.ptr->prev;
         node *tmp = it.ptr->next;
         delete it.ptr;
+        --m_size; // decrement container size
         return iterator(tmp);
     }
     iterator erase(const iterator first, const iterator last) {
@@ -106,6 +109,7 @@ template <typename T> class list {
         for (it = first; it != last; it = tmp) {
             tmp = std::next(it);
             delete it.ptr;
+            --m_size; // decrement container size by 1
         }
         tmp.ptr->prev = first.ptr;
         return tmp;
@@ -113,10 +117,10 @@ template <typename T> class list {
     void pop_front() { erase(begin()); }
     void pop_back() { erase(std::prev(end())); }
 
-    iterator begin() noexcept { return iterator(head->next); }
-    const iterator begin() const noexcept { return iterator(head->next); }
-    iterator end() noexcept { return iterator(tail); }
-    const iterator end() const noexcept { return iterator(tail); }
+    iterator begin() noexcept { return iterator(m_head->next); }
+    const iterator begin() const noexcept { return iterator(m_head->next); }
+    iterator end() noexcept { return iterator(m_tail); }
+    const iterator end() const noexcept { return iterator(m_tail); }
     std::reverse_iterator<iterator> rbegin() noexcept {
         return std::reverse_iterator(end());
     }
@@ -129,8 +133,8 @@ template <typename T> class list {
     const std::reverse_iterator<iterator> rend() const noexcept {
         return std::reverse_iterator(begin());
     }
-    const iterator cbegin() const noexcept { return iterator(head->next); }
-    const iterator cend() const noexcept { return iterator(tail); }
+    const iterator cbegin() const noexcept { return iterator(m_head->next); }
+    const iterator cend() const noexcept { return iterator(m_tail); }
     const std::reverse_iterator<iterator> crbegin() const noexcept {
         return std::reverse_iterator(rbegin());
     }
@@ -144,26 +148,24 @@ template <typename T> class list {
 
     constexpr bool operator!=(const list &rhs) const { return !(*this == rhs); }
 
-    T &operator[](size_t index) {
-        node *ret = head;
-        for (size_t i = 0; i < index; ++i) {
-            ret = head->next;
-        }
-        return ret->element;
-    }
+    T &operator[](size_t index) { return *std::next(begin(), index); }
+
+    size_t size() const noexcept { return m_size; }
+    bool empty() const noexcept { return m_size == 0; }
 
     // copy assignment
     list &operator=(const list &rhs) {
         // free original list
-        for (node *it = head, *tmp; it != tail; it = tmp) {
+        for (node *it = m_head, *tmp; it != m_tail; it = tmp) {
             tmp = it->next;
             delete it;
         }
-        delete tail;
-        head = new node();
-        tail = new node();
-        head->next = tail;
-        tail->prev = head;
+        delete m_tail;
+        m_head = new node();
+        m_tail = new node();
+        m_head->next = m_tail;
+        m_tail->prev = m_head;
+        m_size = rhs.m_size;
         // copy
         if (this != &rhs) {
             for (const auto &v : rhs) {
@@ -176,54 +178,58 @@ template <typename T> class list {
     // move assignment
     list &operator=(list &&rhs) {
         // free original list
-        for (node *it = head, *tmp; it != tail; it = tmp) {
+        for (node *it = m_head, *tmp; it != m_tail; it = tmp) {
             tmp = it->next;
             delete it;
         }
-        delete tail;
-        head = new node();
-        tail = new node();
-        head->next = tail;
-        tail->prev = head;
+        delete m_tail;
+        m_head = new node();
+        m_tail = new node();
+        m_head->next = m_tail;
+        m_tail->prev = m_head;
+        m_size = rhs.m_size;
         // move
         if (this != &rhs) {
-            head = rhs.head;
-            tail = rhs.tail;
-            rhs.head = rhs.tail = nullptr;
+            m_head = rhs.m_head;
+            m_tail = rhs.m_tail;
+            rhs.m_head = rhs.m_tail = nullptr;
         }
         return *this;
     }
 
-    list() : head(new node()), tail(new node()) {
-        head->next = tail;
-        tail->prev = head;
+    list() : m_head(new node()), m_tail(new node()), m_size(0) {
+        m_head->next = m_tail;
+        m_tail->prev = m_head;
     }
-    list(std::initializer_list<T> init) : head(new node()), tail(new node()) {
-        head->next = tail;
-        tail->prev = head;
+    list(std::initializer_list<T> init)
+        : m_head(new node()), m_tail(new node()), m_size(0) {
+        m_head->next = m_tail;
+        m_tail->prev = m_head;
         for (const auto &v : init) {
             emplace_back(v);
         }
     }
 
     // copy constructor
-    list(const list &l) : head(new node()), tail(new node()) {
-        head->next = tail;
-        tail->prev = head;
+    list(const list &l) : m_head(new node()), m_tail(new node()), m_size(0) {
+        m_head->next = m_tail;
+        m_tail->prev = m_head;
         for (const auto &v : l) {
             emplace_back(v);
         }
     }
 
     // move constructor
-    list(list &&l) : head(l.head), tail(l.tail) { l.head = l.tail = nullptr; }
+    list(list &&l) : m_head(l.m_head), m_tail(l.m_tail), m_size(l.m_size) {
+        l.m_head = l.m_tail = nullptr;
+    }
 
     ~list() {
-        for (node *it = head, *tmp; it != tail; it = tmp) {
+        for (node *it = m_head, *tmp; it != m_tail; it = tmp) {
             tmp = it->next;
             delete it;
         }
-        delete tail;
+        delete m_tail;
     }
 };
 } // namespace mystl
