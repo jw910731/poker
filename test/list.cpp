b76efdf5f036abf595243b708dd9ab0af90da9a3
jw910731 <jw910731@gmail.com>
2021-12-05 15:14:30 +0800

    Add iterator & rule of five test
    Also add success hint message for each test pass.

--- test/list.cpp
+++ test/list.cpp
@@ -39,11 +39,44 @@ void test_emplace(mystl::list<std::pair<int, int>> &l,
     }
 }
 
+void test_iterator(mystl::list<std::pair<int, int>> &l,
+                   std::list<std::pair<int, int>> &t) {
+    if (*std::prev(l.end()) != *std::prev(t.end())) {
+        abort();
+    }
+    if (*std::next(l.begin()) != *std::next(t.begin())) {
+        abort();
+    }
+}
+
+void test_rule_of_five(mystl::list<std::pair<int, int>> &l,
+                       std::list<std::pair<int, int>> &t) {
+    mystl::list<std::pair<int, int>> l_copy(l);
+    std::list<std::pair<int, int>> t_copy(t);
+    if (l_copy != l ||
+        !std::equal(l_copy.begin(), l_copy.end(), t.begin(), t.end())) {
+        abort();
+    }
+    t_copy = t;
+    l_copy = l;
+    if (l_copy != l ||
+        !std::equal(l_copy.begin(), l_copy.end(), t.begin(), t.end())) {
+        abort();
+    }
+}
+
 int main() {
     mystl::list<std::pair<int, int>> sample({{1, 11}, {2, 12}, {3, 13}});
     std::list<std::pair<int, int>> testcase({{1, 11}, {2, 12}, {3, 13}});
     test_emplace_front(sample, testcase);
+    std::cout << "[Success] Pass emplace front test\n";
     test_emplace(sample, testcase);
+    std::cout << "[Success] Pass emplace test\n";
     test_emplace_back(sample, testcase);
+    std::cout << "[Success] Pass emplace back test\n";
+    test_rule_of_five(sample, testcase);
+    std::cout << "[Success] Pass copy & move assignment and ctor test\n";
+    test_iterator(sample, testcase);
+    std::cout << "[Success] Pass iterator behaviour test\n";
     return 0;
 }
